c0665527afbe134011b38bfccc8a31891a05ded4
sharuuuuu <sharucaelangarajah@gmail.com>
2025-10-07 18:21:22 +0200

    Feat: translated some comments and add 'markdown' folder in the code

--- src/preprocessing/file_to_md.py
+++ src/preprocessing/file_to_md.py
@@ -2,12 +2,12 @@ from markitdown import MarkItDown
 import os
 from urllib.parse import urlparse
 
-#En fnunksjon som konverter andre fil_typer til md-filer
+#A function that converts other file types to Markdown files
 def file_to_md(input_file: str, output_filename: str):
 
     md = MarkItDown()
     parsed = urlparse(input_file)
-    #for å hente en relativ filsti (slik at vi kan plassere den riktig)
+#To get a relative file path (so we can place it correctly)
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     if parsed.scheme in ("http", "https"):
@@ -15,7 +15,7 @@ def file_to_md(input_file: str, output_filename: str):
 
     else: 
 
-        #bygger file_path ut fra script_dir + input_file
+        #Builds the file_path from script_dir + input_file
         file_path = os.path.abspath(os.path.join(script_dir, input_file))
 
         if not os.path.exists(file_path):
@@ -23,11 +23,11 @@ def file_to_md(input_file: str, output_filename: str):
         
         md_content = md.convert(file_path)
     
-    base_dir = os.path.join(script_dir, "..", "..", "data", "processed")
+    base_dir = os.path.join(script_dir, "..", "..", "data", "markdown")
     os.makedirs(base_dir, exist_ok=True)
     output_path = os.path.join(base_dir, output_filename)
 
-    #skirv/lager ny fil som blir lagt til i processed mappe i documents
+    #Writes/creates a new file that is saved in the 'markdown' folder in Documents
     with open(output_path, "w", encoding="utf-8") as f:
         f.write(md_content.text_content)
 
